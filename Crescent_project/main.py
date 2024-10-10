a7e145935441f2a6558334c6207b6d82ccd516cb
Vinícius souza <vinicanal604@gmail.com>
2024-10-10 17:29:00 -0300

    Crescent v1.7

--- Crescent_project/main.py
+++ Crescent_project/main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 app.secret_key = '3dcdaf74c8761209363e9a75450ba5b0'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@localhost/teste_crescent'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:22012006@localhost/teste_crescent'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 init_app(app)
